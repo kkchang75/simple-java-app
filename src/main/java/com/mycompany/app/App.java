3c0b8bb32753f1e27691edd2f0b07fd33e8b8230
Kok Kuan <kokkchan@gmail.com>
2023-06-01 02:52:32 +0000

    test email

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("hot day");
+        System.out.println("hot day2");
     }
 
     public String getMessage() {
